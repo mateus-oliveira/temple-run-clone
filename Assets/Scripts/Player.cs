7895a0376c3249dde4ee00a7d4f6eee1a4fd71d1
Mateus Oliveira <devmateusalves@gmail.com>
2025-10-07 09:15:07 -0300

    feat: basic moviments

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,57 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animator animator;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody rigidbody;
+    private bool jumping;
+    [SerializeField] private float jumpForce;
+
     void Start()
     {
+        jumping = false;
         animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("Space key was pressed.");
+            jumping = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Slide();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (jumping)
+        {
+            jumping = false;
             animator.SetBool("Jump", true);
+            rigidbody.linearVelocity = Vector3.up * jumpForce;
+        }
+    }
+
+    public void Slide()
+    {
+        animator.SetBool("Slide", true);
+        transform.rotation = Quaternion.Euler(0, 90, 0);
+    }
+
+    public void StopSlide()
+    {
+        animator.SetBool("Slide", false);
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            animator.SetBool("Jump", false);
         }
     }
 }
